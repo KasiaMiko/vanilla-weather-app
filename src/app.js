a04ba0469bc79884e8b31070b900953ea3e97fec
KasiaMiko <74139350+KasiaMiko@users.noreply.github.com>
2020-11-14 20:40:16 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-  function formatDay(timestamp) {
-let date= new Date(timestamp);
-let days = [
+function formatDay(timestamp) {
+    let date= new Date(timestamp);
+    let days = [
         "Sunday",
         "Monday",
         "Tuesday",
@@ -12,14 +12,14 @@ let days = [
       let day= days[date.getDay()];
       return `${day}`;
   }
-  function formatTime(timestamp) {
-let date= new Date(timestamp);
-let hour= date.getHours();
-if (hour <10){
+function formatTime(timestamp) {
+    let date= new Date(timestamp);
+    let hour= date.getHours();
+    if (hour <10){
     hour=`0${hour}`;
 }
-let minutes= date.getMinutes();
-if (minutes <10){
+    let minutes= date.getMinutes();
+    if (minutes <10){
     minutes=`0${minutes}`;
 }
       return `${hour}:${minutes}`;
@@ -43,8 +43,20 @@ if (minutes <10){
       iconElement.setAttribute("alt", response.data.weather[0].description);
   }
 
-  let apiKey = "cac6eb1808e7ad8b2e537949ab1a8c09";
-  let city = "Berlin"
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
+function search (city){
+    let apiKey = "cac6eb1808e7ad8b2e537949ab1a8c09";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayVariables);
+}
+
+  function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement=document.querySelector("#search-text-input");
+    search(cityInputElement.value);
+}
+  
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+  
